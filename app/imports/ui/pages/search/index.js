9425fabdda2fe18bb2810517c199168921a721ce
Iao Seng Sio <tonysio42@gmail.com>
2017-11-21 18:12:41 -1000

    Done

--- app/imports/ui/pages/search/index.js
+++ app/imports/ui/pages/search/index.js
@@ -1,7 +1,8 @@
-import './search-page.css';
 import './search-page.html';
+import './search-page.css';
 import './search-page.js';
 import './search-friends-page.html';
 import './search-friends-page.css';
+import './search-friends-page.js';
 import './event-page.css';
 import './event-page.html';
--- app/imports/ui/pages/search/search-friends-page.js
+++ app/imports/ui/pages/search/search-friends-page.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 
-Template.Search_Page.events({
+Template.Search_Friends_Page.events({
   'click .advanced-search'() {
     $('.advanced-search-form').transition('fly down');
   }
