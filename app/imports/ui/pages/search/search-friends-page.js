2d1636431c04e3417b903309d7ef0fdb5efa2c1a
Iao Seng Sio <tonysio42@gmail.com>
2017-11-30 08:51:52 -1000

    Interactive search popup

--- app/imports/ui/pages/search/search-friends-page.js
+++ app/imports/ui/pages/search/search-friends-page.js
@@ -1,6 +1,54 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
+import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Interests } from '/imports/api/interest/InterestCollection';
+
+const selectedInterestsKey = 'selectedInterests';
+
+Template.Search_Friends_Page.onCreated(function onCreated() {
+  this.subscribe(Profiles.getPublicationName());
+  this.subscribe(Interests.getPublicationName());
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(selectedInterestsKey, undefined);
+});
+
+Template.Search_Friends_Page.helpers({
+  profiles() {
+    // Initialize selectedInterests to all of them if messageFlags is undefined.
+    if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
+      Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
+    }
+    // Find all profiles with the currently selected interests.
+    const allProfiles = Profiles.findAll();
+    const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
+    return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
+  },
+
+  interests() {
+    return _.map(Interests.findAll(),
+        function makeInterestObject(interest) {
+          return {
+            label: interest.name,
+            selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
+          };
+        });
+  },
+});
 
 Template.Search_Friends_Page.events({
+  'click .submit'(event, instance) {
+    event.preventDefault();
+    $('.results-area').transition('slide down');
+    const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
+    instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
+  },
+  'submit .search-friends-form'(event, instance) {
+    event.preventDefault();
+    $('.results-area').transition('slide down');
+    const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
+    instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
+  },
   'click .advanced-search'() {
     $('.advanced-search-form').transition('fly down');
   }
