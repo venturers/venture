9fb1f43027f739ec3d0c8e42dfa5cc4f3176368e
camelial <camelial@hawaii.edu>
2017-12-05 08:51:55 -1000

    events attending section: added helper method event string

---
+++ app/imports/ui/pages/user/public-profile-page.js
@@ -0,0 +1,15 @@
+import { Template } from 'meteor/templating';
+
+Template.Public_Profile_Page.helpers({
+  numFriends(profile) {
+    if (profile.friends) {
+      return profile.friends.length;
+    }
+    return 0;
+  },
+  eventsToString(profile) {
+    if (profile.events) {
+      return _.map(profile.events, event => (event.name + " | " + event.date + " " + event.time));
+    }
+  }
+});
